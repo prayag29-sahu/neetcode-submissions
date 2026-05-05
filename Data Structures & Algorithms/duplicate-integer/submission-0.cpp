e711e157fc3d34fcfb0dce8e5cea4953db3a6571
Prayag Sahu <sahuprayag229@gmail.com>
2026-05-05 22:13:46 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+   public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for (int n : nums) {
+            if(m.find(n)!=m.end()){
+               return true;
+            } 
+            m[n]++;
+        }
+        return false;
+    }
+};
