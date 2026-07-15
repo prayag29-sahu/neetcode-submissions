d2431cb579f2b807988160e5bdb7259a7fc83a3f
Prayag Sahu <sahuprayag229@gmail.com>
2026-07-15 16:41:51 +0530

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+
+        priority_queue<pair<int, vector<int>>> maxHeap;
+
+        for (auto point : points) {
+
+            int x = point[0];
+            int y = point[1];
+
+            int dist = x * x + y * y;
+
+            maxHeap.push({dist, point});
+
+            if (maxHeap.size() > k)
+                maxHeap.pop();
+        }
+
+        vector<vector<int>> ans;
+
+        while (!maxHeap.empty()) {
+
+            ans.push_back(maxHeap.top().second);
+            maxHeap.pop();
+        }
+
+        return ans;
+    }
+};
