255000714e21e22cf7be8d670dbcdd68dd8cb5e6
Prayag Sahu <sahuprayag229@gmail.com>
2026-08-01 23:12:17 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+   public:
+    bool isValid(string s) {
+        stack<int> st;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.empty())
+                    return false;
+                else if (c == ')' && st.top() == '(' || c == '}' && st.top() == '{' ||
+                         c == ']' && st.top() == '[') {
+                    st.pop();
+                } else
+                    return false;
+            }
+        }
+        return st.empty();
+    }
+};
